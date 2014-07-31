3ed3c20370655d06099753b46b55bedc2fab72cc
Geoffrey Stebbins <geoff.stebbins@ssrc.msstate.edu>
2014-07-31 13:12:52 -0500

    split questions to individual template pages

--- sba3/sba3/views/general.py
+++ sba3/sba3/views/general.py
@@ -8,3 +8,29 @@ from django.db.models.loading import get_model
 
 def questions(request):
    return render(request,'questions.html')
+
+def page(request, pagenum):
+    if pagenum=='1':
+        return render(request, 'page1.html')
+    elif pagenum=='2':
+        return render(request, 'page2.html')
+    elif pagenum=='3':
+        return render(request, 'page3.html')
+    elif pagenum=='4':
+        return render(request, 'page4.html')
+    elif pagenum=='5':
+        return render(request, 'page5.html')
+    elif pagenum=='6':
+        return render(request, 'page6.html')
+    elif pagenum=='7':
+        return render(request, 'page7.html')
+    elif pagenum=='8':
+        return render(request, 'page8.html')
+    elif pagenum=='9':
+        return render(request, 'page9.html')
+    elif pagenum=='10':
+        return render(request, 'page10.html')
+    elif pagenum=='11':
+        return render(request, 'page11.html')
+    else:
+        return render(request, 'questions.html')
