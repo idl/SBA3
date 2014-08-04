ad0c774f839907e61ceb415c0f87b569636d6a4d
Geoffrey Stebbins <geoff.stebbins@ssrc.msstate.edu>
2014-08-04 12:00:14 -0500

    forgot to add navigation view

---
+++ sba3/sba3/views/navigation.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+
+from ..models import Answers
+
+def previous(request):
+    pagenum = int(request.POST.get('pagenum', 1))
+
+    # Get the answers and store them in the session
+    array_name = "p" + request.POST.get('pagenum', 1)
+    answer_array = request.POST.getlist(array_name + "[]")
+    request.session[array_name] = answer_array
+
+    # Redirect
+    if pagenum <= 1:
+        return redirect('page', 1, test= 'test')
+    else:
+        return redirect('page', pagenum - 1)
+
+def next(request):
+    pagenum = int(request.POST.get('pagenum', 1))
+
+    # Get the answers and store them in the session
+    array_name = "p" + request.POST.get('pagenum', 1)
+    answer_array = request.POST.getlist(array_name + "[]")
+    request.session[array_name] = answer_array
+
+    # Redirect
+    if pagenum >= 11:
+        return redirect('page', 11)
+    else:
+        return redirect('page', pagenum + 1)
+
+def submit(request):
+    array_name = "p11"
+    answer_array = request.POST.getlist(array_name + "[]")
+    request.session[array_name] = answer_array
+
+    row = {}
+
+    for pagenum in range(1,12):
+        print "pagestart"
+        page = "p" + str(pagenum)
+        print page
+        answer_array = request.session[page]
+        print answer_array
+        answernum = 1
+        for answer in answer_array:
+            print "answerstart"
+            column = page + "q" + str(answernum)
+            print column
+            row[column] = answer
+            answernum = answernum + 1
+
+    print row
+
+    submission = Answers(**row)
+    submission.save()
+
+    request.session.flush()
+
+    return render(request, 'questions.html')
