81d2eec22cd7d943460a723f0c4ab7cdf04d7a21
John Buffington <john.buffington@ssrc.msstate.edu>
2014-08-26 10:45:05 -0500

    More login functionality

--- sba3/admin_custom/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-	error_css_class = "error"
-	required_css_class = "required"
-	label_suffix = ""
-
-	email = forms.EmailField(required=True, label="Email")
-	password = forms.CharField(required=True, label="Password")
