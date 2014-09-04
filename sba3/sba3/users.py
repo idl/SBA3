80eb232af2e5c2bf376fbf75e2cd47206d0fe2d1
John Buffington <john.buffington@ssrc.msstate.edu>
2014-09-04 16:49:16 -0500

    Add register school functionality

---
+++ sba3/sba3/users.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# https://docs.djangoproject.com/en/dev/topics/auth/customizing/
+class Users(AbstractBaseUser):
+    email = models.EmailField(max_length=254, unique=True, blank=False)
+    username = models.CharField(max_length=20, blank=False)
+    display_name = models.CharField(max_length=45, blank=True)
+
+    USERNAME_FIELD = 'email' # specify unique key
+    REQUIRED_FIELDS = ['email', ] # only affects createsuperuser command
+    is_active = True
+
+
+
+    # Required to override these methods, else throw exception when called
+    def get_full_name(self):
+    	if display_name == "":
+    		return self.username
+    	return self.display_name
+    def get_short_name(self):
+    	return self.username
+
+
+ class UsersManager(BaseUserManager):
+ 	def create_user(self):
+ 		now = timezone.now()
