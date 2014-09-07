6fa8bccd7a162f769e017ca53e1a1777a136dfb0
John Gavin Buffington <john.g.buffington@gmail.com>
2014-09-07 14:30:08 -0500

    Removed .form-control class from create admin superuser option

--- sba3/admin_custom/forms.py
+++ sba3/admin_custom/forms.py
@@ -12,8 +12,7 @@ class registerAdminForm(forms.Form):
     email = forms.EmailField(required=True, label='Email', initial='', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(required=True, label='Password', widget=forms.TextInput(attrs={'class': 'form-control', 'type':'password'}))
     password_confirm = forms.CharField(required=True, label='Confirm Password', widget=forms.TextInput(attrs={'class': 'form-control', 'type':'password'}))
-
-    superuser = forms.BooleanField(required=False, label="Global Admin?", widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+    superuser = forms.BooleanField(required=False, label="Global Admin?", widget=forms.CheckboxInput(attrs={'class': ''}))
     school = forms.ModelChoiceField(required=False, label='School', queryset=School.objects.all().order_by('name'), widget=forms.Select(attrs={'class': 'form-control'}))
 
 class registerSchoolUserForm(forms.Form):
