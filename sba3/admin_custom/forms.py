aace66364fbc354f27f5a753e9fa2f56472c2739
grs470 <grs470@gmail.com>
2014-09-08 04:41:17 -0500

    fixed school creation and school display on admin creation

--- sba3/admin_custom/forms.py
+++ sba3/admin_custom/forms.py
@@ -8,13 +8,15 @@ class LoginForm(forms.Form):
     password = forms.CharField(required=True, label='Password')
 
 class registerAdminForm(forms.Form):
+    school_list = School.objects.all().order_by('name')
+
     # school_name = forms.CharField(required=True, label='School Name', initial='')
     email = forms.EmailField(required=True, label='Email', initial='', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(required=True, label='Password', widget=forms.TextInput(attrs={'class': 'form-control', 'type':'password'}))
     password_confirm = forms.CharField(required=True, label='Confirm Password', widget=forms.TextInput(attrs={'class': 'form-control', 'type':'password'}))
 
     superuser = forms.BooleanField(required=False, label="Global Admin?", widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
-    school = forms.ModelChoiceField(required=False, label='School', queryset=School.objects.all().order_by('name'), widget=forms.Select(attrs={'class': 'form-control'}))
+    school = forms.ModelChoiceField(required=False, label='School', queryset=school_list, widget=forms.Select(attrs={'class': 'form-control'}))
 
 class registerSchoolUserForm(forms.Form):
 	school_name = forms.CharField(required=True, label='School Name', initial='')
