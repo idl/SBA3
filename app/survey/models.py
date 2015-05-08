35502f604a522a8f7c71daa44671b1fdb08fdac4
John Gavin Buffington <john.g.buffington@gmail.com>
2015-05-08 11:16:37 -0500

    new models

--- app/survey/models.py
+++ app/survey/models.py
@@ -2,11 +2,18 @@ import hashlib
 from django.db import models
 
 
+class SchoolManager(models.Manager):
+  def has_uid(self, uid):
+    students = Student.objects.filter(school=self.model)
+    print students
+
 class School(models.Model):
   name = models.CharField(max_length=75, blank=False)
   location = models.CharField(max_length=125, help_text='Please enter the city and state of your school.')
   survey_title = models.CharField(max_length=50, null=True)
   date = models.DateField()
+
+  objects = SchoolManager()
 
   def get_survey_years(self):
     print 'THIS SURVEY: ', self.name
@@ -17,52 +24,21 @@ class School(models.Model):
   class Meta:
     db_table = u'School'
 
-class StudentManager(models.Manager):
-  def create_student(self, uid, school):
-    error = ''
-    if not uid:
-      error = 'Student must have a uid'
-    elif not school:
-      error = 'Student must have a school'
 
-    if Student.objects.filter(uid__iexact=uid, school=school).count() > 0:
-      error = 'Student - School association already exists'
-    else:
-      continue_hash = hashlib.sha256(uid + school.name).hexdigest()
-      continue_pass = continue_hash[:10]
 
-      student = self.model(
-        uid=uid,
-        school=school,
-        continue_pass=continue_pass,
-        completed=False
-      )
-      student.save()
+# class UidManager(models.Manager):
+#   def has_uid(self, uid):
+#     students = Student.obj
 
-      return student
-    if error == '':
-      error = 'Unknown Error'
-    return error
-
-class Student(models.Model):
+# 1 Uid can have many Students
+class Uid(models.Model):
   uid = models.CharField(max_length=20)
-  school = models.ForeignKey(School, null=True)
-  continue_pass = models.CharField(max_length=10, blank=False)
-  completed = models.BooleanField()
 
-  objects = StudentManager()
+  def __unicode__(self):
+    return self.uid
 
   class Meta:
-    db_table = u'Student'
-
-
-# class SchoolUid(models.Model):
-#   school = models.ForeignKey(School)
-#   uid = models.CharField(max_length=20)
-
-
-
-
+    db_table = u'Uid'
 
 
 
@@ -207,141 +183,193 @@ class Student(models.Model):
 
 #     return student_total / total_answers
 
-# class ResultSet(models.Model):
-#   uid = models.ForeignKey(Student, db_index = True)
-#   p1q1 = models.CharField(max_length=20, null=True)
-#   p1q2 = models.CharField(max_length=20, null=True)
-#   p1q3 = models.CharField(max_length=20, null=True)
-#   p1q4 = models.CharField(max_length=20, null=True)
-#   p1q5 = models.CharField(max_length=20, null=True)
-#   p1q6 = models.CharField(max_length=20, null=True)
-#   p1q7 = models.CharField(max_length=20, null=True)
-#   p1q8 = models.CharField(max_length=20, null=True)
-#   p1q9 = models.CharField(max_length=20, null=True)
-#   p1q10 = models.CharField(max_length=20, null=True)
-#   p1q11 = models.CharField(max_length=20, null=True)
-#   p2q1 = models.CharField(max_length=20, null=True)
-#   p2q2 = models.CharField(max_length=20, null=True)
-#   p2q3 = models.CharField(max_length=20, null=True)
-#   p2q4 = models.CharField(max_length=20, null=True)
-#   p2q5 = models.CharField(max_length=20, null=True)
-#   p2q6 = models.CharField(max_length=20, null=True)
-#   p2q7 = models.CharField(max_length=20, null=True)
-#   p2q8 = models.CharField(max_length=20, null=True)
-#   p2q9 = models.CharField(max_length=20, null=True)
-#   p2q10 = models.CharField(max_length=20, null=True)
-#   p2q11 = models.CharField(max_length=20, null=True)
-#   p3q1 = models.CharField(max_length=20, null=True)
-#   p3q2 = models.CharField(max_length=20, null=True)
-#   p3q3 = models.CharField(max_length=20, null=True)
-#   p3q4 = models.CharField(max_length=20, null=True)
-#   p3q5 = models.CharField(max_length=20, null=True)
-#   p3q6 = models.CharField(max_length=20, null=True)
-#   p3q7 = models.CharField(max_length=20, null=True)
-#   p3q8 = models.CharField(max_length=20, null=True)
-#   p3q9 = models.CharField(max_length=20, null=True)
-#   p4q1 = models.CharField(max_length=20, null=True)
-#   p4q2 = models.CharField(max_length=20, null=True)
-#   p4q3 = models.CharField(max_length=20, null=True)
-#   p4q4 = models.CharField(max_length=20, null=True)
-#   p4q5 = models.CharField(max_length=20, null=True)
-#   p4q6 = models.CharField(max_length=20, null=True)
-#   p4q7 = models.CharField(max_length=20, null=True)
-#   p4q8 = models.CharField(max_length=20, null=True)
-#   p4q9 = models.CharField(max_length=20, null=True)
-#   p4q10 = models.CharField(max_length=20, null=True)
-#   p4q11 = models.CharField(max_length=20, null=True)
-#   p5q1 = models.CharField(max_length=20, null=True)
-#   p5q2 = models.CharField(max_length=20, null=True)
-#   p5q3 = models.CharField(max_length=20, null=True)
-#   p5q4 = models.CharField(max_length=20, null=True)
-#   p5q5 = models.CharField(max_length=20, null=True)
-#   p5q6 = models.CharField(max_length=20, null=True)
-#   p5q7 = models.CharField(max_length=20, null=True)
-#   p5q8 = models.CharField(max_length=20, null=True)
-#   p5q9 = models.CharField(max_length=20, null=True)
-#   p5q10 = models.CharField(max_length=20, null=True)
-#   p5q11 = models.CharField(max_length=20, null=True)
-#   p5q12 = models.CharField(max_length=20, null=True)
-#   p5q13 = models.CharField(max_length=20, null=True)
-#   p5q14 = models.CharField(max_length=20, null=True)
-#   p6q1 = models.CharField(max_length=20, null=True)
-#   p6q2 = models.CharField(max_length=20, null=True)
-#   p6q3 = models.CharField(max_length=20, null=True)
-#   p6q4 = models.CharField(max_length=20, null=True)
-#   p6q5 = models.CharField(max_length=20, null=True)
-#   p6q6 = models.CharField(max_length=20, null=True)
-#   p6q7 = models.CharField(max_length=20, null=True)
-#   p6q8 = models.CharField(max_length=20, null=True)
-#   p6q9 = models.CharField(max_length=20, null=True)
-#   p6q10 = models.CharField(max_length=20, null=True)
-#   p6q11 = models.CharField(max_length=20, null=True)
-#   p6q12 = models.CharField(max_length=20, null=True)
-#   p7q1 = models.CharField(max_length=20, null=True)
-#   p7q2 = models.CharField(max_length=20, null=True)
-#   p7q3 = models.CharField(max_length=20, null=True)
-#   p7q4 = models.CharField(max_length=20, null=True)
-#   p7q5 = models.CharField(max_length=20, null=True)
-#   p7q6 = models.CharField(max_length=20, null=True)
-#   p7q7 = models.CharField(max_length=20, null=True)
-#   p7q8 = models.CharField(max_length=20, null=True)
-#   p7q9 = models.CharField(max_length=20, null=True)
-#   p8q1 = models.CharField(max_length=20, null=True)
-#   p8q2 = models.CharField(max_length=20, null=True)
-#   p8q3 = models.CharField(max_length=20, null=True)
-#   p8q4 = models.CharField(max_length=20, null=True)
-#   p8q5 = models.CharField(max_length=20, null=True)
-#   p8q6 = models.CharField(max_length=20, null=True)
-#   p8q7 = models.CharField(max_length=20, null=True)
-#   p8q8 = models.CharField(max_length=20, null=True)
-#   p8q9 = models.CharField(max_length=20, null=True)
-#   p8q10 = models.CharField(max_length=20, null=True)
-#   p8q11 = models.CharField(max_length=20, null=True)
-#   p8q12 = models.CharField(max_length=20, null=True)
-#   p9q1 = models.CharField(max_length=20, null=True)
-#   p9q2 = models.CharField(max_length=20, null=True)
-#   p9q3 = models.CharField(max_length=20, null=True)
-#   p9q4 = models.CharField(max_length=20, null=True)
-#   p9q5 = models.CharField(max_length=20, null=True)
-#   p9q6 = models.CharField(max_length=20, null=True)
-#   p9q7 = models.CharField(max_length=20, null=True)
-#   p9q8 = models.CharField(max_length=20, null=True)
-#   p9q9 = models.CharField(max_length=20, null=True)
-#   p9q10 = models.CharField(max_length=20, null=True)
-#   p9q11 = models.CharField(max_length=20, null=True)
-#   p9q12 = models.CharField(max_length=20, null=True)
-#   p9q13 = models.CharField(max_length=20, null=True)
-#   p10q1 = models.CharField(max_length=2, null=True)
-#   p10q2 = models.CharField(max_length=2, null=True)
-#   p10q3 = models.CharField(max_length=2, null=True)
-#   p10q4 = models.CharField(max_length=2, null=True)
-#   p10q5 = models.CharField(max_length=2, null=True)
-#   p10q6 = models.CharField(max_length=2, null=True)
-#   p10q7 = models.TextField(max_length=255, null=True)
-#   p10q8 = models.TextField(max_length=255, null=True)
-#   p10q9 = models.TextField(max_length=255, null=True)
-#   p10q10 = models.TextField(max_length=255, null=True)
-#   p10q11 = models.TextField(max_length=255, null=True)
-#   p10q12 = models.TextField(max_length=255, null=True)
-#   p11q1 = models.CharField(max_length=20, null=True)
-#   p11q2 = models.CharField(max_length=20, null=True)
-#   p11q3 = models.CharField(max_length=20, null=True)
-#   p11q4 = models.CharField(max_length=20, null=True)
-#   p11q5 = models.CharField(max_length=20, null=True)
-#   p11q6 = models.CharField(max_length=20, null=True)
-#   p11q7 = models.CharField(max_length=20, null=True)
-#   p11q8 = models.CharField(max_length=20, null=True)
-#   p11q9 = models.CharField(max_length=20, null=True)
-#   p11q10 = models.CharField(max_length=20, null=True)
-#   p11q11 = models.CharField(max_length=20, null=True)
-#   p11q12 = models.CharField(max_length=20, null=True)
-#   p11q13 = models.CharField(max_length=20, null=True)
-#   p11q14 = models.CharField(max_length=20, null=True)
+class ResultSet(models.Model):
+  p1q1 = models.CharField(max_length=20, null=True)
+  p1q2 = models.CharField(max_length=20, null=True)
+  p1q3 = models.CharField(max_length=20, null=True)
+  p1q4 = models.CharField(max_length=20, null=True)
+  p1q5 = models.CharField(max_length=20, null=True)
+  p1q6 = models.CharField(max_length=20, null=True)
+  p1q7 = models.CharField(max_length=20, null=True)
+  p1q8 = models.CharField(max_length=20, null=True)
+  p1q9 = models.CharField(max_length=20, null=True)
+  p1q10 = models.CharField(max_length=20, null=True)
+  p1q11 = models.CharField(max_length=20, null=True)
+  p2q1 = models.CharField(max_length=20, null=True)
+  p2q2 = models.CharField(max_length=20, null=True)
+  p2q3 = models.CharField(max_length=20, null=True)
+  p2q4 = models.CharField(max_length=20, null=True)
+  p2q5 = models.CharField(max_length=20, null=True)
+  p2q6 = models.CharField(max_length=20, null=True)
+  p2q7 = models.CharField(max_length=20, null=True)
+  p2q8 = models.CharField(max_length=20, null=True)
+  p2q9 = models.CharField(max_length=20, null=True)
+  p2q10 = models.CharField(max_length=20, null=True)
+  p2q11 = models.CharField(max_length=20, null=True)
+  p3q1 = models.CharField(max_length=20, null=True)
+  p3q2 = models.CharField(max_length=20, null=True)
+  p3q3 = models.CharField(max_length=20, null=True)
+  p3q4 = models.CharField(max_length=20, null=True)
+  p3q5 = models.CharField(max_length=20, null=True)
+  p3q6 = models.CharField(max_length=20, null=True)
+  p3q7 = models.CharField(max_length=20, null=True)
+  p3q8 = models.CharField(max_length=20, null=True)
+  p3q9 = models.CharField(max_length=20, null=True)
+  p4q1 = models.CharField(max_length=20, null=True)
+  p4q2 = models.CharField(max_length=20, null=True)
+  p4q3 = models.CharField(max_length=20, null=True)
+  p4q4 = models.CharField(max_length=20, null=True)
+  p4q5 = models.CharField(max_length=20, null=True)
+  p4q6 = models.CharField(max_length=20, null=True)
+  p4q7 = models.CharField(max_length=20, null=True)
+  p4q8 = models.CharField(max_length=20, null=True)
+  p4q9 = models.CharField(max_length=20, null=True)
+  p4q10 = models.CharField(max_length=20, null=True)
+  p4q11 = models.CharField(max_length=20, null=True)
+  p5q1 = models.CharField(max_length=20, null=True)
+  p5q2 = models.CharField(max_length=20, null=True)
+  p5q3 = models.CharField(max_length=20, null=True)
+  p5q4 = models.CharField(max_length=20, null=True)
+  p5q5 = models.CharField(max_length=20, null=True)
+  p5q6 = models.CharField(max_length=20, null=True)
+  p5q7 = models.CharField(max_length=20, null=True)
+  p5q8 = models.CharField(max_length=20, null=True)
+  p5q9 = models.CharField(max_length=20, null=True)
+  p5q10 = models.CharField(max_length=20, null=True)
+  p5q11 = models.CharField(max_length=20, null=True)
+  p5q12 = models.CharField(max_length=20, null=True)
+  p5q13 = models.CharField(max_length=20, null=True)
+  p5q14 = models.CharField(max_length=20, null=True)
+  p6q1 = models.CharField(max_length=20, null=True)
+  p6q2 = models.CharField(max_length=20, null=True)
+  p6q3 = models.CharField(max_length=20, null=True)
+  p6q4 = models.CharField(max_length=20, null=True)
+  p6q5 = models.CharField(max_length=20, null=True)
+  p6q6 = models.CharField(max_length=20, null=True)
+  p6q7 = models.CharField(max_length=20, null=True)
+  p6q8 = models.CharField(max_length=20, null=True)
+  p6q9 = models.CharField(max_length=20, null=True)
+  p6q10 = models.CharField(max_length=20, null=True)
+  p6q11 = models.CharField(max_length=20, null=True)
+  p6q12 = models.CharField(max_length=20, null=True)
+  p7q1 = models.CharField(max_length=20, null=True)
+  p7q2 = models.CharField(max_length=20, null=True)
+  p7q3 = models.CharField(max_length=20, null=True)
+  p7q4 = models.CharField(max_length=20, null=True)
+  p7q5 = models.CharField(max_length=20, null=True)
+  p7q6 = models.CharField(max_length=20, null=True)
+  p7q7 = models.CharField(max_length=20, null=True)
+  p7q8 = models.CharField(max_length=20, null=True)
+  p7q9 = models.CharField(max_length=20, null=True)
+  p8q1 = models.CharField(max_length=20, null=True)
+  p8q2 = models.CharField(max_length=20, null=True)
+  p8q3 = models.CharField(max_length=20, null=True)
+  p8q4 = models.CharField(max_length=20, null=True)
+  p8q5 = models.CharField(max_length=20, null=True)
+  p8q6 = models.CharField(max_length=20, null=True)
+  p8q7 = models.CharField(max_length=20, null=True)
+  p8q8 = models.CharField(max_length=20, null=True)
+  p8q9 = models.CharField(max_length=20, null=True)
+  p8q10 = models.CharField(max_length=20, null=True)
+  p8q11 = models.CharField(max_length=20, null=True)
+  p8q12 = models.CharField(max_length=20, null=True)
+  p9q1 = models.CharField(max_length=20, null=True)
+  p9q2 = models.CharField(max_length=20, null=True)
+  p9q3 = models.CharField(max_length=20, null=True)
+  p9q4 = models.CharField(max_length=20, null=True)
+  p9q5 = models.CharField(max_length=20, null=True)
+  p9q6 = models.CharField(max_length=20, null=True)
+  p9q7 = models.CharField(max_length=20, null=True)
+  p9q8 = models.CharField(max_length=20, null=True)
+  p9q9 = models.CharField(max_length=20, null=True)
+  p9q10 = models.CharField(max_length=20, null=True)
+  p9q11 = models.CharField(max_length=20, null=True)
+  p9q12 = models.CharField(max_length=20, null=True)
+  p9q13 = models.CharField(max_length=20, null=True)
+  p10q1 = models.CharField(max_length=2, null=True)
+  p10q2 = models.CharField(max_length=2, null=True)
+  p10q3 = models.CharField(max_length=2, null=True)
+  p10q4 = models.CharField(max_length=2, null=True)
+  p10q5 = models.CharField(max_length=2, null=True)
+  p10q6 = models.CharField(max_length=2, null=True)
+  p10q7 = models.TextField(max_length=255, null=True)
+  p10q8 = models.TextField(max_length=255, null=True)
+  p10q9 = models.TextField(max_length=255, null=True)
+  p10q10 = models.TextField(max_length=255, null=True)
+  p10q11 = models.TextField(max_length=255, null=True)
+  p10q12 = models.TextField(max_length=255, null=True)
+  p11q1 = models.CharField(max_length=20, null=True)
+  p11q2 = models.CharField(max_length=20, null=True)
+  p11q3 = models.CharField(max_length=20, null=True)
+  p11q4 = models.CharField(max_length=20, null=True)
+  p11q5 = models.CharField(max_length=20, null=True)
+  p11q6 = models.CharField(max_length=20, null=True)
+  p11q7 = models.CharField(max_length=20, null=True)
+  p11q8 = models.CharField(max_length=20, null=True)
+  p11q9 = models.CharField(max_length=20, null=True)
+  p11q10 = models.CharField(max_length=20, null=True)
+  p11q11 = models.CharField(max_length=20, null=True)
+  p11q12 = models.CharField(max_length=20, null=True)
+  p11q13 = models.CharField(max_length=20, null=True)
+  p11q14 = models.CharField(max_length=20, null=True)
 
-#   objects = ResultSetManager()
+  # objects = ResultSetManager()
 
-#   def __unicode__(self):
-#     return 'Answer Set'
+  def __unicode__(self):
+    return 'Answer Set'
 
-#   class Meta:
-#     db_table = 'SurveyResultSet'
+  class Meta:
+    db_table = 'SurveyResultSet'
+
+
+
+
+
+
+class StudentManager(models.Manager):
+  def create_student(self, uid, school):
+    error = ''
+    if not uid:
+      error = 'Student must have a uid'
+    elif not school:
+      error = 'Student must have a school'
+
+    if Student.objects.filter(uid__iexact=uid, school=school).count() > 0:
+      error = 'Student - School association already exists'
+    else:
+      continue_hash = hashlib.sha256(uid + school.name).hexdigest()
+      continue_pass = continue_hash[:10]
+
+      student = self.model(
+        uid=uid,
+        school=school,
+        continue_pass=continue_pass,
+        completed=False
+      ).save()
+
+      return student
+    if error == '':
+      error = 'Unknown Error'
+    return error
+
+
+class Student(models.Model):
+  school = models.ForeignKey(School)
+  continue_pass = models.CharField(max_length=10, blank=False)
+  completed = models.BooleanField()
+  result_set = models.OneToOneField(ResultSet, null=True)
+  uid = models.ForeignKey(Uid) # 1 Uid -> n Students (multiple
+                               # students fromdifferent schools can have the
+                               #  same school Uid)
+  objects = StudentManager()
+
+  def __unicode__(self):
+    return str(self.uid) + ", " + str(self.school)
+
+  class Meta:
+    db_table = u'Student'
+
+
+
+
+
--- app/survey/views.py
+++ app/survey/views.py
@@ -13,7 +13,8 @@ from .forms.questions_page_8 import QuestionsPage8Form
 from .forms.questions_page_9 import QuestionsPage9Form
 from .forms.questions_page_10 import QuestionsPage10Form
 from .forms.questions_page_11 import QuestionsPage11Form
-from .models import Student, School
+from .models import Student, School, Uid
+from django.contrib.auth.decorators import user_passes_test
 
 forms = {
   '1': QuestionsPage1Form,
@@ -63,10 +64,24 @@ def public_continue(request):
 
 def questions(request, school_id, student_uid, page_num):
   context = {}
+
+  # for q_num in range(1, num_questions_on_page[page_num]+1):
+  #   if ResultSet for this page is empty
+  #     redirect to this page with page_num=page_num-1
+
+  if request.session.get('student_uid') != student_uid or request.session.get('school_id') != school_id:
+    print "doesn't match session"
+
+  try:
+    school = School.objects.get(id=school_id)
+    uid = Uid.objects.get(uid=student_uid)
+    Student.objects.get(school=school, uid=uid)
+  except:
+    messages.error(request, 'Could not process your request.')
+    return redirect('public_survey_begin')
+
   if request.POST:
     form = forms[page_num](request.POST)
-    request.session['school_id'] = school_id
-    request.session['student_uid'] = student_uid
     if form.is_valid():
       request.session['next_page_num'] = int(page_num) + 1
     else:
@@ -90,9 +105,8 @@ def next(request):
   next_page_num = int(request.session.get('next_page_num'))
   school_id = request.session.get('school_id')
   student_uid = request.session.get('student_uid')
-  for q in range(1, num_questions_on_page[next_page_num]+1):
+  for q in range(1, num_questions_on_page[str(next_page_num)]+1):
     res = request.session.get('page_results_q'+str(q))
-    print "## ", res
   return redirect('survey_questions', school_id, student_uid, next_page_num)
 
 def previous(request):
@@ -116,13 +130,19 @@ def public_begin(request):
     context['survey_begin_form'] = form
     student = None
     try:
-      student = Student.objects.get(uid=student_uid, school=School.objects.get(id=school_id))
+      # print Uid.objects.filter(uid=student_uid)
+      student = Student.objects.get(
+        uid=Uid.objects.get(uid=student_uid), school=School.objects.get(id=school_id)
+      )
     except:
       messages.error(request, 'The user ID "'+student_uid+'" is not registered with this school.')
       return render(request, "survey/survey_begin.html", context)
     if student.completed:
       messages.error(request, 'The student "'+student_uid+'" has already completed the survey.')
       return render(request, "survey/survey_begin.html", context)
+    request.session['student_uid'] = student_uid
+    request.session['school_id'] = school_id
+    print "STUDENT: ", student
     return redirect('survey_questions', school_id, student_uid, 1)
   return render(request, "survey/survey_begin.html", context)
 
