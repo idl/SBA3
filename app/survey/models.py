7456efebac031ada52f390bf03da4d0bc5d47faf
John Gavin Buffington <john.g.buffington@gmail.com>
2015-05-11 02:19:08 -0500

    validation + save result logic

--- app/survey/models.py
+++ app/survey/models.py
@@ -1,6 +1,7 @@
 import hashlib
+import collections
 from django.db import models
-
+from jsonfield import JSONField
 
 class SchoolManager(models.Manager):
   def has_uid(self, uid):
@@ -11,7 +12,7 @@ class School(models.Model):
   name = models.CharField(max_length=75, blank=False)
   location = models.CharField(max_length=125, help_text='Please enter the city and state of your school.')
   survey_title = models.CharField(max_length=50, null=True)
-  date = models.DateField()
+  date_created = models.DateField()
 
   objects = SchoolManager()
 
@@ -184,142 +185,26 @@ class Uid(models.Model):
 #     return student_total / total_answers
 
 class ResultSet(models.Model):
-  p1q1 = models.CharField(max_length=20, null=True)
-  p1q2 = models.CharField(max_length=20, null=True)
-  p1q3 = models.CharField(max_length=20, null=True)
-  p1q4 = models.CharField(max_length=20, null=True)
-  p1q5 = models.CharField(max_length=20, null=True)
-  p1q6 = models.CharField(max_length=20, null=True)
-  p1q7 = models.CharField(max_length=20, null=True)
-  p1q8 = models.CharField(max_length=20, null=True)
-  p1q9 = models.CharField(max_length=20, null=True)
-  p1q10 = models.CharField(max_length=20, null=True)
-  p1q11 = models.CharField(max_length=20, null=True)
-  p2q1 = models.CharField(max_length=20, null=True)
-  p2q2 = models.CharField(max_length=20, null=True)
-  p2q3 = models.CharField(max_length=20, null=True)
-  p2q4 = models.CharField(max_length=20, null=True)
-  p2q5 = models.CharField(max_length=20, null=True)
-  p2q6 = models.CharField(max_length=20, null=True)
-  p2q7 = models.CharField(max_length=20, null=True)
-  p2q8 = models.CharField(max_length=20, null=True)
-  p2q9 = models.CharField(max_length=20, null=True)
-  p2q10 = models.CharField(max_length=20, null=True)
-  p2q11 = models.CharField(max_length=20, null=True)
-  p3q1 = models.CharField(max_length=20, null=True)
-  p3q2 = models.CharField(max_length=20, null=True)
-  p3q3 = models.CharField(max_length=20, null=True)
-  p3q4 = models.CharField(max_length=20, null=True)
-  p3q5 = models.CharField(max_length=20, null=True)
-  p3q6 = models.CharField(max_length=20, null=True)
-  p3q7 = models.CharField(max_length=20, null=True)
-  p3q8 = models.CharField(max_length=20, null=True)
-  p3q9 = models.CharField(max_length=20, null=True)
-  p4q1 = models.CharField(max_length=20, null=True)
-  p4q2 = models.CharField(max_length=20, null=True)
-  p4q3 = models.CharField(max_length=20, null=True)
-  p4q4 = models.CharField(max_length=20, null=True)
-  p4q5 = models.CharField(max_length=20, null=True)
-  p4q6 = models.CharField(max_length=20, null=True)
-  p4q7 = models.CharField(max_length=20, null=True)
-  p4q8 = models.CharField(max_length=20, null=True)
-  p4q9 = models.CharField(max_length=20, null=True)
-  p4q10 = models.CharField(max_length=20, null=True)
-  p4q11 = models.CharField(max_length=20, null=True)
-  p5q1 = models.CharField(max_length=20, null=True)
-  p5q2 = models.CharField(max_length=20, null=True)
-  p5q3 = models.CharField(max_length=20, null=True)
-  p5q4 = models.CharField(max_length=20, null=True)
-  p5q5 = models.CharField(max_length=20, null=True)
-  p5q6 = models.CharField(max_length=20, null=True)
-  p5q7 = models.CharField(max_length=20, null=True)
-  p5q8 = models.CharField(max_length=20, null=True)
-  p5q9 = models.CharField(max_length=20, null=True)
-  p5q10 = models.CharField(max_length=20, null=True)
-  p5q11 = models.CharField(max_length=20, null=True)
-  p5q12 = models.CharField(max_length=20, null=True)
-  p5q13 = models.CharField(max_length=20, null=True)
-  p5q14 = models.CharField(max_length=20, null=True)
-  p6q1 = models.CharField(max_length=20, null=True)
-  p6q2 = models.CharField(max_length=20, null=True)
-  p6q3 = models.CharField(max_length=20, null=True)
-  p6q4 = models.CharField(max_length=20, null=True)
-  p6q5 = models.CharField(max_length=20, null=True)
-  p6q6 = models.CharField(max_length=20, null=True)
-  p6q7 = models.CharField(max_length=20, null=True)
-  p6q8 = models.CharField(max_length=20, null=True)
-  p6q9 = models.CharField(max_length=20, null=True)
-  p6q10 = models.CharField(max_length=20, null=True)
-  p6q11 = models.CharField(max_length=20, null=True)
-  p6q12 = models.CharField(max_length=20, null=True)
-  p7q1 = models.CharField(max_length=20, null=True)
-  p7q2 = models.CharField(max_length=20, null=True)
-  p7q3 = models.CharField(max_length=20, null=True)
-  p7q4 = models.CharField(max_length=20, null=True)
-  p7q5 = models.CharField(max_length=20, null=True)
-  p7q6 = models.CharField(max_length=20, null=True)
-  p7q7 = models.CharField(max_length=20, null=True)
-  p7q8 = models.CharField(max_length=20, null=True)
-  p7q9 = models.CharField(max_length=20, null=True)
-  p8q1 = models.CharField(max_length=20, null=True)
-  p8q2 = models.CharField(max_length=20, null=True)
-  p8q3 = models.CharField(max_length=20, null=True)
-  p8q4 = models.CharField(max_length=20, null=True)
-  p8q5 = models.CharField(max_length=20, null=True)
-  p8q6 = models.CharField(max_length=20, null=True)
-  p8q7 = models.CharField(max_length=20, null=True)
-  p8q8 = models.CharField(max_length=20, null=True)
-  p8q9 = models.CharField(max_length=20, null=True)
-  p8q10 = models.CharField(max_length=20, null=True)
-  p8q11 = models.CharField(max_length=20, null=True)
-  p8q12 = models.CharField(max_length=20, null=True)
-  p9q1 = models.CharField(max_length=20, null=True)
-  p9q2 = models.CharField(max_length=20, null=True)
-  p9q3 = models.CharField(max_length=20, null=True)
-  p9q4 = models.CharField(max_length=20, null=True)
-  p9q5 = models.CharField(max_length=20, null=True)
-  p9q6 = models.CharField(max_length=20, null=True)
-  p9q7 = models.CharField(max_length=20, null=True)
-  p9q8 = models.CharField(max_length=20, null=True)
-  p9q9 = models.CharField(max_length=20, null=True)
-  p9q10 = models.CharField(max_length=20, null=True)
-  p9q11 = models.CharField(max_length=20, null=True)
-  p9q12 = models.CharField(max_length=20, null=True)
-  p9q13 = models.CharField(max_length=20, null=True)
-  p10q1 = models.CharField(max_length=2, null=True)
-  p10q2 = models.CharField(max_length=2, null=True)
-  p10q3 = models.CharField(max_length=2, null=True)
-  p10q4 = models.CharField(max_length=2, null=True)
-  p10q5 = models.CharField(max_length=2, null=True)
-  p10q6 = models.CharField(max_length=2, null=True)
-  p10q7 = models.TextField(max_length=255, null=True)
-  p10q8 = models.TextField(max_length=255, null=True)
-  p10q9 = models.TextField(max_length=255, null=True)
-  p10q10 = models.TextField(max_length=255, null=True)
-  p10q11 = models.TextField(max_length=255, null=True)
-  p10q12 = models.TextField(max_length=255, null=True)
-  p11q1 = models.CharField(max_length=20, null=True)
-  p11q2 = models.CharField(max_length=20, null=True)
-  p11q3 = models.CharField(max_length=20, null=True)
-  p11q4 = models.CharField(max_length=20, null=True)
-  p11q5 = models.CharField(max_length=20, null=True)
-  p11q6 = models.CharField(max_length=20, null=True)
-  p11q7 = models.CharField(max_length=20, null=True)
-  p11q8 = models.CharField(max_length=20, null=True)
-  p11q9 = models.CharField(max_length=20, null=True)
-  p11q10 = models.CharField(max_length=20, null=True)
-  p11q11 = models.CharField(max_length=20, null=True)
-  p11q12 = models.CharField(max_length=20, null=True)
-  p11q13 = models.CharField(max_length=20, null=True)
-  p11q14 = models.CharField(max_length=20, null=True)
+  load_kwargs = {'object_pairs_hook': collections.OrderedDict}
+  p1 = JSONField(load_kwargs=load_kwargs, null=True)
+  p2 = JSONField(load_kwargs=load_kwargs, null=True)
+  p3 = JSONField(load_kwargs=load_kwargs, null=True)
+  p4 = JSONField(load_kwargs=load_kwargs, null=True)
+  p5 = JSONField(load_kwargs=load_kwargs, null=True)
+  p6 = JSONField(load_kwargs=load_kwargs, null=True)
+  p7 = JSONField(load_kwargs=load_kwargs, null=True)
+  p8 = JSONField(load_kwargs=load_kwargs, null=True)
+  p9 = JSONField(load_kwargs=load_kwargs, null=True)
+  p10 = JSONField(load_kwargs=load_kwargs, null=True)
+  p11 = JSONField(load_kwargs=load_kwargs, null=True)
 
   # objects = ResultSetManager()
 
   def __unicode__(self):
-    return 'Answer Set'
+    return 'ResultSet: ' + str(self.id)
 
   class Meta:
-    db_table = 'SurveyResultSet'
+    db_table = 'ResultSet'
 
 
 
@@ -362,6 +247,12 @@ class Student(models.Model):
                                # students fromdifferent schools can have the
                                #  same school Uid)
   objects = StudentManager()
+
+  def has_started_survey(self):
+    if self.result_set_id is None or self.result_set is None or self.result_set.p1 is None:
+      return False
+    else:
+      return True
 
   def __unicode__(self):
     return str(self.uid) + ", " + str(self.school)
