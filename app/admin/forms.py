94bada59c82047cbd0a91989fdb24fe3e6ca62af
John Gavin Buffington <john.g.buffington@gmail.com>
2015-05-20 11:25:36 -0500

    remove all django default admin stuff

--- app/admin/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class AdminLoginForm(forms.Form):
-  email = forms.EmailField(label="Email")
-  password = forms.CharField(label="Password", max_length=254, widget=forms.PasswordInput)
--- app/admin/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from .forms import AdminLoginForm
-
-def public_login(request):
-  context = {
-    'admin_login_form': AdminLoginForm(),
-  }
-  return render(request, "admin/public_login.html", context)
