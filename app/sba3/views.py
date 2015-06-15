da19efca06a06b6f380f2aec3b42450fab5039c2
John Gavin Buffington <john.g.buffington@gmail.com>
2015-06-14 22:55:26 -0500

    comment

--- app/sba3/views.py
+++ app/sba3/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+# public index view
 def public_index(request):
   return render(request, "public/index.html")
 
